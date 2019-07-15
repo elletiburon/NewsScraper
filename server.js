1ab5bed97b9119e635c7b6c8328e29935140ddb6
elletiburon <elleharrer@gmail.com>
2019-07-15 13:34:20 -0500

    scrape works

--- server.js
+++ server.js
@@ -20,19 +20,19 @@ app.use(express.json());
 app.use(express.static("public"));
 
 
-mongoose.connect("mongodb://localhost/unit18Populater", { useNewUrlParser: true });
+mongoose.connect("mongodb://localhost/NewsScraperDB", { useNewUrlParser: true });
 
 //ROUTES
 
-// A GET route for scraping the echoJS website
+
 app.get("/scrape", function(req, res) {
     // First, we grab the body of the html with axios
-    axios.get("http://www.echojs.com/").then(function(response) {
+    axios.get("https://laughingsquid.com/").then(function(response) {
       // Then, we load that into cheerio and save it to $ for a shorthand selector
       var $ = cheerio.load(response.data);
   
       // Now, we grab every h2 within an article tag, and do the following:
-      $("article h2").each(function(i, element) {
+      $(".entry-title").each(function(i, element) {
         // Save an empty result object
         var result = {};
   
